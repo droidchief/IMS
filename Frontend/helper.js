db0572153be299c95fd0fb8630b076b7cfadf290
Victor Loveday <56683410+Vicviral@users.noreply.github.com>
2021-10-05 15:45:45 +0100

    Feat: get current time and date

--- Frontend/helper.js
+++ Frontend/helper.js
@@ -3,3 +3,11 @@ let backButton = document.querySelector('.backButton')
 backButton.addEventListener('click', () => {
     window.history.back()
 })
+
+//Get current date
+var today = new Date();
+var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+
+//Get current time
+var today = new Date();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
