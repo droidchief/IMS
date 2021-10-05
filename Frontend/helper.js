0b4e79df01e26b9fbd3b1f5ca9a87822636a5d0f
Victor Loveday <56683410+Vicviral@users.noreply.github.com>
2021-10-05 16:25:47 +0100

    Feat: update UI with current date and time

--- Frontend/helper.js
+++ Frontend/helper.js
@@ -1,13 +1,19 @@
 // Back button
 let backButton = document.querySelector('.backButton')
-backButton.addEventListener('click', () => {
-    window.history.back()
-})
+if (backButton != null) {
+    backButton.addEventListener('click', () => {
+        window.history.back()
+    })
+}
 
-//Get current date
+// Get current date and time
 var today = new Date();
-var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-
-//Get current time
-var today = new Date();
+var date = today.getFullYear('y') + '-' + (today.getMonth('M') + 1) + '-' + today.getDate();
 var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+// Update current time and date in the UI
+var currentDate = document.getElementById("currentDate")
+currentDate.innerHTML = date
+
+var currentTime = document.getElementById("currentTime")
+currentTime.innerHTML = time
